9e511bc736268bd11b085b1f0076242693f692b1
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-01-30 21:11:03 +0300

    Add mobile Menu in HTML and some JS

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const mainMenu = document.querySelector('.mobile-menu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.menu');
+const menu_items = document.querySelectorAll('.mobile-menu li a');
