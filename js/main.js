077a895c1acd6c6f30feadf92708fff060e870f5
Walid Kabboussa <walidkabou@gmail.com>
2023-01-30 19:23:43 +0100

    Add Javascript function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ const mainMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.menu');
 const menu_items = document.querySelectorAll('.mobile-menu li a');
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
