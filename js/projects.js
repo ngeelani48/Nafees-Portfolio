a28c64fde4b9e9049cdf82f87f9423fec4a5ad1e
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-01 16:15:34 +0300

    updata javascript and remove linters

--- js/projects.js
+++ js/projects.js
@@ -1,79 +1,111 @@
-const array =[
-    {
-        id:'1',
-        name: 'Multi-Post-Stories',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/Project 1.png',
-        tech: ['css', 'html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio'
-    },
-    {
-        id:'2',
-        name: 'Profesional Art Printing Data',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/Project2.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio'
-    },
-    {
-        id:'3',
-        name: 'Data Dashboard Healthcare',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/desktop-project3.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio' 
-    },
-    {
-        id:'4',
-        name: 'Website Protfolio',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/desktop-project4.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio' 
-    },
-    {
-        id:'5',
-        name: 'Profesional Art Printing Data',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/Project2.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio' 
-    },
-    {
-        id:'6',
-        name: 'Data Dashboard Healthcare',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/desktop-project3.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio' 
-    },
-    {
-        id:'7',
-        name: 'Website Portfolio',
-        shortdescription: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-        longdescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.`,
-        image: './images/desktop-project4.png',
-        tech: ['html', 'bootstrap', 'Ruby'],
-        liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
-        sourcelink: 'https://github.com/ngeelani48/My-Portfolio' 
-    }
-]
-const project=document.querySelector('#works-section');
-const details=document.querySelector('.popup-details');
+const array = [
+  {
+    id: '1',
+    name: 'Multi-Post-Stories',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/Project 1.png',
+    tech: ['css', 'html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '2',
+    name: 'Profesional Art Printing Data',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/Project2.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '3',
+    name: 'Data Dashboard Healthcare',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/desktop-project3.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '4',
+    name: 'Website Protfolio',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/desktop-project4.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '5',
+    name: 'Profesional Art Printing Data',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/Project2.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '6',
+    name: 'Data Dashboard Healthcare',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/desktop-project3.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+  {
+    id: '7',
+    name: 'Website Portfolio',
+    shortdescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    image: './images/desktop-project4.png',
+    tech: ['html', 'bootstrap', 'Ruby'],
+    liveproject: 'https://ngeelani48.github.io/My-Portfolio/',
+    sourcelink: 'https://github.com/ngeelani48/My-Portfolio',
+  },
+];
+const project = document.querySelector('#works-section');
+const details = document.querySelector('.popup-details');
 
-project.innerHTML= `<div class="project1">
+function closepopup() {
+  details.style.visibility = 'hidden';
+}
+
+function popup(id) {
+  details.style.visibility = 'visible';
+  for (let j = 0; j < array.length; j += 1) {
+    if (id === array[j].id) {
+      details.innerHTML = `<div class="project-details">
+              <h2 class="details-title">${array[j].name}</h2>
+              <div class="details-cross" onclick="closepopup()"><img src="/images/blackcross.png" alt=""></div>
+              <ul class="details-tech">
+                  <li><button>${array[j].tech[0]}</button></li>
+                  <li><button>${array[j].tech[1]}</button></li>
+                  <li><button>${array[j].tech[2]}</button></li>
+              </ul>
+              <div class="details-content">
+              <img class="details-image" src="${array[j].image}" alt="Project image">
+              <div class="details-text">
+              <p class="details-description">${array[j].longdescription}</p>
+              <ul class="details-button">
+              <li><button>See Live <img src="/images/icon-seelive.png" alt="see live icon"></button></li>
+              <li><button>See Source <img src="/images/github.png" alt="github icon"></button></li>
+              </ul>
+              </div>
+              </div>
+          </div>`;
+    }
+  }
+}
+popup();
+closepopup();
+project.innerHTML = `<div class="project1">
 <div class="project1-picture">
     <img src="${array[0].image}" alt="project1 image">
 </div>
@@ -89,8 +121,8 @@ project.innerHTML= `<div class="project1">
     <button id="${array[0].id}" onclick="popup(this.id)" class="project1-button">See Project</button>
 </div>
 </div>`;
-for (let i=1; i<array.length;i++){
-    project.innerHTML += `<div class="project2" style="background-image: url('${array[i].image}'); background-repeat: no-repeat; background-size: cover;">
+for (let i = 1; i < array.length; i += 1) {
+  project.innerHTML += `<div class="project2" style="background-image: url('${array[i].image}'); background-repeat: no-repeat; background-size: cover;">
     <h1 class="project2-title">${array[i].name}</h1>
     <p class="project2-description">${array[i].shortdescription}</p>
         <ul class="project2-tech">
@@ -100,34 +132,4 @@ for (let i=1; i<array.length;i++){
         </ul>
         <button id="${array[i].id}" onclick="popup(this.id)" class="project2-button">See Project</button>  
 </div>`;
-}
-function popup(id){
-    details.style.visibility='visible';
-    for(let j=0; j<array.length; j++){
-        if (id===array[j].id){
-            details.innerHTML=`<div class="project-details">
-            <h2 class="details-title">${array[j].name}</h2>
-            <div class="details-cross" onclick="closepopup()"><img src="/images/blackcross.png" alt=""></div>
-            <ul class="details-tech">
-                <li><button>${array[j].tech[0]}</button></li>
-                <li><button>${array[j].tech[1]}</button></li>
-                <li><button>${array[j].tech[2]}</button></li>
-            </ul>
-            <div class="details-content">
-            <img class="details-image" src="${array[j].image}" alt="Project image">
-            <div class="details-text">
-            <p class="details-description">${array[j].longdescription}</p>
-            <ul class="details-button">
-            <li><button>See Live <img src="/images/icon-seelive.png" alt="see live icon"></button></li>
-            <li><button>See Source <img src="/images/github.png" alt="github icon"></button></li>
-            </ul>
-            </div>
-            </div>
-        </div>`;
-        }
-    }
-}
-
-function closepopup() {
-    details.style.visibility='hidden';
 }
