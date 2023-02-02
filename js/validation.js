0be84d8de3e9821b08e0ed239e9cf512241eced6
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-02 14:51:34 +0300

    fix linters

--- js/validation.js
+++ js/validation.js
@@ -1,12 +1,11 @@
-
 document.querySelector('.button-contactform').addEventListener('click', (e) => {
-    const email = document.getElementById('email').value;
-    const errorEmail = document.querySelector('#err');
-  
-    const right = (str) => str === str.toLowerCase();
-    if (!right(email)) {
-      e.preventDefault();
-      errorEmail.textContent = 'Please check if your email is in lowercase letters!!!';
-      errorEmail.style.background='red';
-    }
-  });
+  const email = document.getElementById('email').value;
+  const errorEmail = document.querySelector('#err');
+
+  const right = (str) => str === str.toLowerCase();
+  if (!right(email)) {
+    e.preventDefault();
+    errorEmail.textContent = 'Please check if your email is in lowercase letters!!!';
+    errorEmail.style.background = 'red';
+  }
+});
