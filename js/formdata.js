d63ee9a8797fcb03c3cc2eafcd9a66490661f09b
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-02 20:48:40 +0300

    add js for saving form input in local storage

---
+++ js/formdata.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#contact-input');
+const store = [];
+form.addEventListener('input', (ev) => {
+  ev.preventDefault();
+  const data = {
+    id: Date.now,
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('text-contactform').value,
+  };
+  store.push(data);
+  localStorage.setItem('InputData', JSON.stringify(data));
+});
+const retrivedata = localStorage.getItem('InputData');
+const getdata = JSON.parse(retrivedata);
+window.addEventListener('load', () => {
+  if (localStorage.getItem('InputData')) {
+    document.getElementById('name').value = getdata.name;
+    document.getElementById('email').value = getdata.email;
+    document.getElementById('text-contactform').value = getdata.message;
+  }
+});
