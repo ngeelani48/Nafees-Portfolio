1c7de0848885f431a4c915a2d168f0ec1b6ba411
Walid Kabboussa <119979641+Hisoka37@users.noreply.github.com>
2023-02-02 11:44:29 +0000

    Add format validation conditon

--- js/validation.js
+++ js/validation.js
@@ -4,4 +4,9 @@ document.querySelector('.button-contactform').addEventListener('click', (e) => {
     const errorEmail = document.querySelector('#err');
   
     const right = (str) => str === str.toLowerCase();
-});
+    if (!right(email)) {
+      e.preventDefault();
+      errorEmail.textContent = 'Please check if your email is in lowercase letters!!!';
+      errorEmail.style.background='red';
+    }
+  });
